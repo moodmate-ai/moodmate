6fa55d2354ed4ce5cadfc7a990fb59181097273f
hiroto <lg001210@gmail.com>
2025-04-03 19:40:49 +0900

    add page

--- frontend/src/components/LoginPage.tsx
+++ frontend/src/components/LoginPage.tsx
@@ -26,7 +26,7 @@ const LoginPage: React.FC<LoginPageProps> = ({ onLogin, onGoogleLogin }) => {
 
     try {
       onLogin(username, password);
-      navigate('/');
+      navigate('/dashboard');
     } catch (err) {
       setError('로그인에 실패했습니다. 아이디와 비밀번호를 확인해주세요.');
     }
