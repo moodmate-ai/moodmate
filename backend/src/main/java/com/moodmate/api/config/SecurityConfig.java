b0a49fb214407f0f9d616f25b0a47c2526615ba7
H4N9ER <ehdgk0900@gmail.com>
2025-04-29 21:52:27 +0900

    chore: update configs not to authorize

--- backend/src/main/java/com/moodmate/api/config/SecurityConfig.java
+++ backend/src/main/java/com/moodmate/api/config/SecurityConfig.java
@@ -3,11 +3,9 @@ package com.moodmate.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,19 +13,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(CsrfConfigurer::disable)
-            .httpBasic(HttpBasicConfigurer::disable)
+        http
+            .csrf(auth -> auth.disable())
+            .formLogin(auth -> auth.disable())
+            .httpBasic(auth -> auth.disable())
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
-                .requestMatchers("/swagger-ui/**").permitAll()
-                .requestMatchers("/login").permitAll()
-                .anyRequest().authenticated())
-            /*.oauth2Login(Customizer.withDefaults())*/;
+/*              .requestMatchers(
+                    "/",
+                    "/login",
+                    "/swagger-ui/**",
+                    "/swagger-resources/**",
+                    "/api-docs").permitAll()*/
+                .anyRequest().permitAll());
 
         return http.build();
     }
