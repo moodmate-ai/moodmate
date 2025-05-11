087d066aca4053856428545c53bbe3b60a902a31
Wonbin Kim <kwb0711@gmail.com>
2025-05-11 17:15:39 +0900

    Add backend google oauth2 jwt setting

--- backend/src/main/java/com/moodmate/api/config/SecurityConfig.java
+++ backend/src/main/java/com/moodmate/api/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.moodmate.api.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,14 +10,39 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import com.moodmate.api.service.JwtAuthService;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+
+        JwtDecoder jwtDecoder = NimbusJwtDecoder.withJwkSetUri(issuerUri).build();
+
+        return new JwtDecoder() {
+            @Override
+            public Jwt decode(String token) throws JwtException {
+                System.out.println("token: " + token);
+                Jwt jwt = jwtDecoder.decode(token);
+                System.out.println("jwt: " + jwt);
+                return jwt;
+            }
+        };
     }
 
     @Bean
@@ -27,14 +53,21 @@ public class SecurityConfig {
             .httpBasic(auth -> auth.disable())
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(authorize -> authorize
-/*              .requestMatchers(
+            .authorizeHttpRequests(authorize -> 
+                authorize.requestMatchers(
                     "/",
                     "/login",
                     "/swagger-ui/**",
                     "/swagger-resources/**",
-                    "/api-docs").permitAll()*/
-                .anyRequest().permitAll());
+                    "/api-docs"
+                )
+                .permitAll()
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> jwt
+                    .jwtAuthenticationConverter(new JwtAuthService())
+                )
+            );
 
         return http.build();
     }
