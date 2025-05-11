fd1b3e7b7982c49f8830d608e65cc8895de4c422
Wonbin Kim <kwb0711@gmail.com>
2025-05-11 23:31:54 +0900

    Hotfix: disable cors auth

--- backend/src/main/java/com/moodmate/api/config/SecurityConfig.java
+++ backend/src/main/java/com/moodmate/api/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(auth -> auth.disable())
+            .cors(auth -> auth.disable())
             .formLogin(auth -> auth.disable())
             .httpBasic(auth -> auth.disable())
             .sessionManagement(session ->
